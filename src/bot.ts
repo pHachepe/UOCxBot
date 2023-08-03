a5d6349a546b0619bb8a6e7d693397fea55fc92c
PeHachePe <pehachepe@uoc.edu>
2023-08-03 20:34:34 +0200

    Primera Prueba Migración Bot.

--- src/bot.ts
+++ src/bot.ts
@@ -1,101 +1,102 @@
 import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { chunk } from "lodash";
 import express from "express";
-import { applyTextEffect, Variant } from "./textEffects";
+import https from "https"
+import { debugMsg, findBestSubjectMatch, findMatchingSubjects, normalize, parse2Msg, parseResJson2ArrayObjects, parseSubjects2InlineMsg } from "./functions";
 
-import type { Variant as TextEffectVariant } from "./textEffects";
+const CACHE_TIME = 86400
 
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
+if (!process.env.URL_SHEET_JSON) throw new Error("Please add a URL GSheet")
+const urlGSheet: string = process.env.URL_SHEET_JSON
+
+if (!process.env.MSG_HELP) throw new Error("Please add a MSG HELP")
+const msgHelp: string = process.env.MSG_HELP || "No está definido un mensaje de ayuda"
+
+if (!process.env.DEBUG_ID) throw new Error("Please add a DEBUG ID")
+const debugID: number = Number(process.env.DEBUG_ID)
+
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
 
-// Handle the /effect command to apply text effects using an inline keyboard
-type Effect = { code: TextEffectVariant; label: string };
-const allEffects: Effect[] = [
+bot.command("effect", (ctx) => ctx.reply('Not implemented yet'));
+
+// Suggest commands in the menu
+bot.api.setMyCommands([
+  { command: "yo", description: "Be greeted by the bot" },
   {
-    code: "w",
-    label: "Monospace",
+    command: "effect",
+    description: "Apply text effects on the text. (usage: /effect [text])",
   },
-  {
-    code: "b",
-    label: "Bold",
-  },
-  {
-    code: "i",
-    label: "Italic",
-  },
-  {
-    code: "d",
-    label: "Doublestruck",
-  },
-  {
-    code: "o",
-    label: "Circled",
-  },
-  {
-    code: "q",
-    label: "Squared",
-  },
-];
+]);
 
-const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) =>
-  `effect-${effectCode}`;
-
-const effectsKeyboardAccessor = (effectCodes: string[]) => {
-  const effectsAccessor = (effectCodes: string[]) =>
-    effectCodes.map((code) =>
-      allEffects.find((effect) => effect.code === code)
-    );
-  const effects = effectsAccessor(effectCodes);
-
-  const keyboard = new InlineKeyboard();
-  const chunkedEffects = chunk(effects, 3);
-  for (const effectsChunk of chunkedEffects) {
-    for (const effect of effectsChunk) {
-      effect &&
-        keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
-    }
-    keyboard.row();
-  }
-
-  return keyboard;
-};
-
-const textEffectResponseAccessor = (
-  originalText: string,
-  modifiedText?: string
-) =>
-  `Original: ${originalText}` +
-  (modifiedText ? `\nModified: ${modifiedText}` : "");
-
-const parseTextEffectResponse = (
-  response: string
-): {
-  originalText: string;
-  modifiedText?: string;
-} => {
-  const originalText = (response.match(/Original: (.*)/) as any)[1];
-  const modifiedTextMatch = response.match(/Modified: (.*)/);
-
-  let modifiedText;
-  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
-
-  if (!modifiedTextMatch) return { originalText };
-  else return { originalText, modifiedText };
-};
-
-bot.command("effect", (ctx) =>
-  ctx.reply(textEffectResponseAccessor(ctx.match), {
-    reply_markup: effectsKeyboardAccessor(
-      allEffects.map((effect) => effect.code)
-    ),
-  })
+// Handle the /about command
+const aboutUrlKeyboard = new InlineKeyboard().url(
+  "FAQ Ingeniería Informática",
+  "https://sites.google.com/view/faq-enginyeria-informatica"
 );
 
+// Handle all other messages and the /start command
+const introductionMessage = `Hello! I'm a Telegram bot.
+I'm powered by Cyclic, the next-generation serverless computing platform.
+
+<b>Commands</b>
+/yo - Be greeted by me
+/effect [text] - Show a keyboard to apply text effects to [text]`;
+
+const replyWithIntro = (ctx: any) =>
+  ctx.reply(introductionMessage, {
+    reply_markup: aboutUrlKeyboard,
+    parse_mode: "HTML",
+  });
+
+bot.command("start", replyWithIntro);
+bot.on("message", replyWithIntro);
+
+bot.hears(/(.+)/s, async (ctx) => {
+  const inputTxt = normalize(ctx.match[0])
+
+  await https.get(urlGSheet, (res: any) => {
+      const data: Uint8Array[] = []
+      res.on('data', (chunk: Uint8Array) => data.push(chunk))
+      res.on('end', () => {
+          const subjects = parseResJson2ArrayObjects(data)
+          const subject = findBestSubjectMatch(inputTxt, subjects)
+          const msg = parse2Msg(subject) ?? `No he encontrado ninguna coincidencia para "${inputTxt}"\n\n ${msgHelp}`
+          ctx.reply(msg)
+          debugMsg({bot, query: ctx.message, response: msg, debugID})
+      })
+  })
+})
+
+bot.on("inline_query", async (ctx) => {
+    let inputQuery = ctx.inlineQuery?.query;
+    if (!inputQuery) {
+      await ctx.answerInlineQuery([], {
+        cache_time: CACHE_TIME,
+      });
+      return;
+    }
+
+    const queryNormalized = normalize(inputQuery);
+
+    await https.get(urlGSheet, (res: any) => {
+      const data: Uint8Array[] = [];
+      res.on('data', (chunk: Uint8Array) => data.push(chunk));
+      res.on('end', () => {
+        const subjects = parseResJson2ArrayObjects(data);
+        let bestSubjects = findMatchingSubjects(queryNormalized, subjects);
+        bestSubjects = bestSubjects.sort((a, b) => b.rating - a.rating).slice(0, 5);
+        const msg = parseSubjects2InlineMsg(bestSubjects);
+        ctx.answerInlineQuery(msg, { is_personal: false, cache_time: CACHE_TIME });
+        debugMsg({ bot, query: inputQuery, response: JSON.stringify(msg), debugID });
+      });
+    });
+})
+
+/*
 // Handle inline queries
-const queryRegEx = /effect (monospace|bold|italic) (.*)/;
 bot.inlineQuery(queryRegEx, async (ctx) => {
   const fullQuery = ctx.inlineQuery.query;
   const fullQueryMatch = fullQuery.match(queryRegEx);
@@ -128,60 +129,10 @@ Modified: ${modifiedText}`,
     { cache_time: 30 * 24 * 3600 } // one month in seconds
   );
 });
+*/
 
 // Return empty result list for other queries.
-bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
-
-// Handle text effects from the effect keyboard
-for (const effect of allEffects) {
-  const allEffectCodes = allEffects.map((effect) => effect.code);
-
-  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
-    const { originalText } = parseTextEffectResponse(ctx.msg?.text || "");
-    const modifiedText = applyTextEffect(originalText, effect.code);
-
-    await ctx.editMessageText(
-      textEffectResponseAccessor(originalText, modifiedText),
-      {
-        reply_markup: effectsKeyboardAccessor(
-          allEffectCodes.filter((code) => code !== effect.code)
-        ),
-      }
-    );
-  });
-}
-
-// Handle the /about command
-const aboutUrlKeyboard = new InlineKeyboard().url(
-  "Host your own bot for free.",
-  "https://cyclic.sh/"
-);
-
-// Suggest commands in the menu
-bot.api.setMyCommands([
-  { command: "yo", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
-]);
-
-// Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
-
-const replyWithIntro = (ctx: any) =>
-  ctx.reply(introductionMessage, {
-    reply_markup: aboutUrlKeyboard,
-    parse_mode: "HTML",
-  });
-
-bot.command("start", replyWithIntro);
-bot.on("message", replyWithIntro);
+// bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
 
 // Start the server
 if (process.env.NODE_ENV === "production") {
---
+++ src/functions.ts
@@ -0,0 +1,119 @@
+import { findBestMatch } from "string-similarity"
+import { Markup } from "telegraf"
+import { InlineQueryResultArticle } from "telegraf/typings/telegram-types"
+import { Telegraf } from "telegraf";
+
+
+// Delete Accents, etc
+const normalize = (str: string) => str?.normalize("NFD")?.replace(/[\u0300-\u036f]/g, "").toUpperCase() || ''
+
+const parseResJson2ArrayObjects = (data: readonly Uint8Array[]) => {
+    // Invalid Json is received, it's necessary to remove 47 chars at the beginning and 2 at the end
+    const subjectsTxt = Buffer.concat(data).toString().slice(47, -2)
+    const subjectsJson = JSON.parse(subjectsTxt)
+
+    const tableJson = subjectsJson.table
+    const rowsJson = tableJson.rows
+
+    let cellsJson = rowsJson.map((row: any) => row.c)
+    cellsJson = cellsJson.slice(2) // Delete table headers
+
+    let subjects = cellsJson.map((cells: any, id: number) => {
+        const [cat, es, url] = cells.map((cell: any) => cell?.v)
+        return { id, cat, es, url };
+    })
+
+    subjects = subjects.filter((subject: any) => subject.url) // Delete subjects without group url
+
+    return subjects
+}
+
+const findBestSubjectMatch = (inputTxt: string, subjects: { id: number, es: string, cat: string, url: string }[]) => {
+    const subjectsNamesEs = subjects.map(subject => this.normalize(subject.es))
+    const subjectsNamesCat = subjects.map(subject => this.normalize(subject.cat))
+
+    const matchesEs = findBestMatch(inputTxt, subjectsNamesEs)
+    const matchesCat = findBestMatch(inputTxt, subjectsNamesCat)
+    const bestMatches = matchesEs.bestMatch.rating >= matchesCat.bestMatch.rating ? matchesEs : matchesCat
+
+    const subjectMatch = subjects[bestMatches.bestMatchIndex]
+
+    return { ...subjectMatch, ...bestMatches.bestMatch }
+}
+
+const findMatchingSubjects = (inputTxt: string, subjects: { id: number, es: string, cat: string, url: string }[]) => {
+    const subjectsNamesEs = subjects.map(subject => this.normalize(subject.es))
+    const subjectsNamesCat = subjects.map(subject => this.normalize(subject.cat))
+
+    const matchesEs = findBestMatch(inputTxt, subjectsNamesEs)
+    const matchesCat = findBestMatch(inputTxt, subjectsNamesCat)
+
+    const matchsEsCat = [...matchesEs.ratings, ...matchesCat.ratings]
+    const topMatchesEsCat = matchsEsCat.filter(subject => subject.rating > 0).sort((a, b) => b.rating - a.rating)
+
+    const topTargetsEsCat = topMatchesEsCat.map(m => m.target)
+
+    const bestSubjectsEsCat = subjects.filter(
+        subject => topTargetsEsCat.includes(this.normalize(subject.es))
+            || topTargetsEsCat.includes(this.normalize(subject.cat))
+    )
+
+    const bestSubjectsEsCatWithRating = bestSubjectsEsCat.map(item => {
+        let rating: any = topMatchesEsCat.find(m => m.target == this.normalize(item.es) || m.target == this.normalize(item.cat))
+        rating = rating.rating
+        return { ...item, rating }
+    })
+
+    return bestSubjectsEsCatWithRating
+}
+
+const parse2Msg = (subject: { id: number, es: string, cat: string, target: string, url: string, rating: number }) => {
+    let msg = null
+
+    if (subject.rating > 0)
+        msg = ' Id: ' + subject.id
+            + '\nEs: ' + subject.es
+            + '\nCat: ' + subject.cat
+            + '\nRating: ' + subject.rating.toFixed(2)
+            + '\nGroup: ' + subject.url
+
+    return msg
+}
+
+const parseSubjects2InlineMsg = (subjects: any[]): InlineQueryResultArticle[] => {
+    return subjects.map(this.parseSubject2InlineMsg)
+}
+
+const parseSubject2InlineMsg = (subject: any): InlineQueryResultArticle => {
+    let article: InlineQueryResultArticle = {
+        type: "article",
+        id: subject.id,
+        title: subject.es + ' - Rating: ' + subject.rating.toFixed(2),
+        input_message_content: {
+            message_text: ' Id: ' + subject.id
+                + '\nEs: ' + subject.es
+                + '\nCat: ' + subject.cat
+                + '\nRating: ' + subject.rating.toFixed(2)
+                + '\nGroup: ' + subject.url
+        },
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    Markup.button.url('Group 🧿', subject.url),
+                    Markup.button.url('General 🧜🏻', 'https://t.me/joinchat/U7otjvMzawEpEwe4'),
+                    Markup.button.url('UOCxBot 🤖', 'https://t.me/UOCxBot'),
+                    // Markup.button.url('UOC ❤️', 'https://uoc.edu'),
+                ]
+            ]
+        }
+    }
+
+    return article
+}
+
+const debugMsg = ({bot, query = 'Query Empty', response= 'Response Empty', debugID}: { bot: Telegraf, query: any, response: string, debugID: number }): void => {
+    bot.telegram.sendMessage(debugID, "Mensaje: " + JSON.stringify(query));
+    bot.telegram.sendMessage(debugID, "File content at: " + new Date() + " is: \n" + response);
+}
+
+export { normalize, parseResJson2ArrayObjects, findBestSubjectMatch, findMatchingSubjects, parse2Msg, parseSubjects2InlineMsg, parseSubject2InlineMsg, debugMsg }
